6693b8f2a4e7455e4a093fcbcaf0a4cd38594a76
yasyasyu <yasyu.hitoha@gmail.com>
2020-12-09 16:26:39 +0900

    tabindent

--- send.py
+++ send.py
@@ -15,47 +15,47 @@ URL = URLfile.readline()
 URLfile.close()
 
 def postData(data,size):
-    if(data is None):
-        print("params is empty")
-        return False
-    datedata = collect.OrderedDict()
-    send = list()
-    for i in range(size):
-        senddata = collect.OrderedDict()
+	if(data is None):
+		print("params is empty")
+		return False
+	datedata = collect.OrderedDict()
+	send = list()
+	for i in range(size):
+		senddata = collect.OrderedDict()
 
-        datetime = data[i][0]
-        senddata["time"] = datetime[11:]
-        if(data[i][1] == "True"):
-            senddata["check"] = "入室"
-        elif(data[i][1] == "False"):
-            senddata["check"] = "退室"
-        else:
-            return False
-        senddata["ID"] = data[i][2]
-        send.append(senddata)
-    datedata["date"] = datetime[:10]
-    datedata["info"] = send
-    print("{}".format(json.dumps(datedata,indent=4)))
+		datetime = data[i][0]
+		senddata["time"] = datetime[11:]
+		if(data[i][1] == "True"):
+			senddata["check"] = "入室"
+		elif(data[i][1] == "False"):
+			senddata["check"] = "退室"
+		else:
+			return False
+		senddata["ID"] = data[i][2]
+		send.append(senddata)
+	datedata["date"] = datetime[:10]
+	datedata["info"] = send
+	print("{}".format(json.dumps(datedata,indent=4)))
 
-    headers = {
-        'Content-Type': 'application/json',
-    }
+	headers = {
+		'Content-Type': 'application/json',
+	}
 
-    response = requests.post(URL, data=json.dumps(datedata), headers=headers)
-    if(response.status_code == 200 and response.text == "connect"):
-        print(response.text)
-        print("post success!")
-        return True
-    print(response.text)
-    print("post failed")
-    return False
+	response = requests.post(URL, data=json.dumps(datedata), headers=headers)
+	if(response.status_code == 200 and response.text == "connect"):
+		print(response.text)
+		print("post success!")
+		return True
+	print(response.text)
+	print("post failed")
+	return False
 
 if __name__ == "__main__":       
-    i = 0
-    data = []
-    fp = open(LOG, 'r')
-    for line in fp.readlines():
-        log = list(map(str,line.rstrip().split(",")))
-        print(log)
-        data.append(log)
-    postData(data,len(data))
+	i = 0
+	data = []
+	fp = open(LOG, 'r')
+	for line in fp.readlines():
+		log = list(map(str,line.rstrip().split(",")))
+		print(log)
+		data.append(log)
+	postData(data,len(data))
